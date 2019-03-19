952fec4b1f3119fbc974e08a975bca8c52d28b81
Yusuke Naito <yusuke-naito@haveagood.holiday>
2019-03-19 23:14:08 +0900

    [p55] 3.6.3 その他の形式の決まったフォーマットの文字列の解析

---
+++ ch_3/samples/s_6/ss_3/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"strings"
+)
+
+var csvSource = `01101,"060  ","0600000","ﾎｯｶｲﾄﾞｳ","ｻｯﾎﾟﾛｼﾁｭｳｵｳｸ","ｲｶﾆｹｲｻｲｶﾞﾅｲﾊﾞｱｲ","北海道","札幌市中央区","以下に掲載がない場合",0,0,0,0,0,0
+01101,"064  ","0640941","ﾎｯｶｲﾄﾞｳ","ｻｯﾎﾟﾛｼﾁｭｳｵｳｸ","ｱｻﾋｶﾞｵｶ","北海道","札幌市中央区","旭ケ丘",0,0,1,0,0,0
+01101,"060  ","0600041","ﾎｯｶｲﾄﾞｳ","ｻｯﾎﾟﾛｼﾁｭｳｵｳｸ","ｵｵﾄﾞｵﾘﾋｶﾞｼ","北海道","札幌市中央区","大通東",0,0,1,0,0,0`
+
+func main() {
+	reader := strings.NewReader(csvSource)
+	csvReader := csv.NewReader(reader)
+	for {
+		line, err := csvReader.Read()
+		if err == io.EOF {
+			break
+		}
+		fmt.Println(line[2], line[6:9])
+	}
+}
