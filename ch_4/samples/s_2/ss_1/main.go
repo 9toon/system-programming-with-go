96b54d4a1538eb7808b03e3f23005367ec297fd5
Yusuke Naito <yusuke-naito@haveagood.holiday>
2019-03-24 11:51:46 +0900

    [p67-68] 4.2.1 チャネルの使用方法

---
+++ ch_4/samples/s_2/ss_1/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("start sub()")
+
+	// 終了を受け取るためのチャネル
+	done := make(chan bool)
+	go func() {
+		fmt.Println("sub() is running")
+		time.Sleep(time.Second)
+		fmt.Println("sub() is finished")
+		done <- true
+	}()
+
+	// 終了を待つ
+	<-done
+	fmt.Println("all tasks are finished")
+}
